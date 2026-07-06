6211c09b1407d28c2bd77caaf94c52f1b50c9324
liuye11 <liuye11@MacBook-Pro-3.local>
2026-07-06 11:39:02 +0800

    更改端口号

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const {webhookRoutes} = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // 中间件
 app.use(cors());
